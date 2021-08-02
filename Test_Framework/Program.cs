4bf74f804414f9fc10a86192cabcc524389c067c
Максим Иванов <tarakah@gmail.com>
2021-08-02 23:42:55 +0300

    installing restsharp, configuring petstore

---
+++ Test_Framework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RestSharp;
+
+namespace Test_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://petstore.swagger.io/v2");
+            var request = new RestRequest("user/login?username=max&password=123456789", Method.GET);
+            var response = client.Get(request);
+
+        }
+    }
+}

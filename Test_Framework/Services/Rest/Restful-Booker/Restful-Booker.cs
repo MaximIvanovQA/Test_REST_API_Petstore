3690464122db4c167533a4ca3b2084b4daa3440a
Максим Иванов <tarakah@gmail.com>
2021-09-18 01:22:29 +0300

    Добавлен новый клас для взаимодействия с https://restful-booker.herokuapp.com/auth

---
+++ Test_Framework/Services/Rest/Restful-Booker/Restful-Booker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Framework.Services.Rest.Restful_Booker
+{
+    /// <summary>
+    /// Класc для работы с api Restful_Booker 
+    /// (https://restful-booker.herokuapp.com/apidoc/index.html#api-Auth)
+    /// </summary>
+    /// <author>Maksim Ivanov</author>
+    /// <createDate>17.09.2021</createDate>
+
+    class Restful_Booker
+    {
+    }
+}
